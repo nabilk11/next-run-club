25392a992bd56487139446c8c029eca0ffa4174b
nabilk11 <khan.nabiln11.com>
2023-07-23 14:03:27 -0400

    updated app layout tailwind styling and added pf pic placeholder to header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello Run Club</h1>
-    </main>
+    <div className="flex min-h-screen flex-col relative bg-slate-900 text-white">
+      <Header />
+      <main className="flex-1 flex flex-col p-4">
+        <h1>Hello Run Club</h1>
+      </main>
+      <Footer />
+    </div>
   );
 }
