779c8abe15d7489ef1e3d5c27dd6bafe99c7f0ab
nabilk11 <khan.nabiln11.com>
2023-07-23 14:01:09 -0400

    created basic header & footer layout/outline

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="w-full left-0 bg-green-300 border-b border-solid border-white">
+      <h1 className="text-3xl sm:text-6xl">Footer</h1>
+    </div>
+  );
+}
