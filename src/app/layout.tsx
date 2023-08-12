8201b074ec0d243745c553c636412a45916da8a5
nabilk11 <khan.nabiln11.com>
2023-08-12 03:20:19 -0400

    wrapped redux provider onto app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
+import { ReduxProvider } from "./redux/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,9 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header loggedIn={loggedIn} />
-        {children}
-        <Footer />
+        <ReduxProvider>
+          <Header loggedIn={loggedIn} />
+          {children}
+          <Footer />
+        </ReduxProvider>
       </body>
     </html>
   );
